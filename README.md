be15761bf70fb8373bf230a2d752499b0e043192
Celestin4 <nteziryayocelestin4@gmail.com>
2024-02-13 19:15:36 +0200

    setting up ui, login, dashboard, and signup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # my-brand-frontend
+
+# CSS BREAKPOINT
+
+        ## Extra Small Devices (Phones): Up to 576px
+        ## Small Devices (Tablets): 576px - 768px
+        ## Medium Devices (Laptops): 768px - 992px
+        ## Large Devices (Desktops): 992px - 1200px
+        ## Extra Large Devices (Large Desktops): 1200px and above
---
+++ main.js
@@ -0,0 +1,55 @@
+// let circularProgress = document.querySelector(".circular-progress"),
+//     progressValue = document.querySelector(".progress-value");
+
+// let progressStartValue = 0,    
+//     progressEndValue = 90,    
+//     speed = 100;
+    
+// let progress = setInterval(() => {
+//     progressStartValue++;
+
+//     progressValue.textContent = `${progressStartValue}%`
+//     circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #ededed 0deg)`
+
+//     if(progressStartValue == progressEndValue){
+//         clearInterval(progress);
+//     }    
+// }, speed);
+
+
+
+var swiper = new Swiper(".testimonial-swipper", {
+  pagination: {
+    el: ".swiper-pagination",
+    // type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+      640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+      },
+      768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+      },
+    
+  }
+});
